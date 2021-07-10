072c1dc5c78c042cc647d8ae3e7e631b859e3d71
dasurahul <dasurahul598@gmail.com>
2021-07-10 06:35:29 +0530

    Second Commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../logo.svg";
+import MenuIcon from "@material-ui/icons/Menu";
 
 import styled from "styled-components";
 
@@ -7,13 +8,50 @@ const Container = styled.div`
   max-width: 1000px;
   margin-left: auto;
   margin-right: auto;
-  padding-top: 30px;
-  padding-bottom: 30px;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  display: flex;
+  align-items: center;
+  @media (max-width: 800px) {
+    padding: 20px;
+  }
 `;
 
 const Logo = styled.div``;
 
-const NavLink = styled.a``;
+const NavLinksContainer = styled.div`
+  @media (max-width: 800px) {
+    display: none;
+  }
+`;
+
+const NavLink = styled.a`
+  text-decoration: none;
+  font-weight: 700;
+  color: var(--grayish-violet);
+  &:hover {
+    color: var(--dark-violet);
+  }
+  transition: color 300ms;
+`;
+
+const SignUp = styled(NavLink)`
+  color: #fff;
+  background-color: var(--cyan);
+  padding: 12px 26px;
+  border-radius: 25px;
+  &:hover {
+    background-color: var(--light-cyan);
+    color: #fff;
+  }
+  transition: background-color 300ms;
+`;
+
+const IconContainer = styled.div`
+  @media (min-width: 800px) {
+    display: none;
+  }
+`;
 
 const Navbar = () => {
   return (
@@ -21,11 +59,26 @@ const Navbar = () => {
       <Logo>
         <img src={logo} alt="logo" />
       </Logo>
-      <div>
-        <NavLink href="#">Features</NavLink>
-        <NavLink href="#">Pricing</NavLink>
-        <NavLink href="#">Resources</NavLink>
-      </div>
+      <NavLinksContainer style={{ marginLeft: "35px", marginRight: "35px" }}>
+        <NavLink href="#" style={{ marginLeft: "15px", marginRight: "15px" }}>
+          Features
+        </NavLink>
+        <NavLink href="#" style={{ marginLeft: "15px", marginRight: "15px" }}>
+          Pricing
+        </NavLink>
+        <NavLink href="#" style={{ marginLeft: "15px", marginRight: "15px" }}>
+          Resources
+        </NavLink>
+      </NavLinksContainer>
+      <NavLinksContainer style={{ marginLeft: "auto" }}>
+        <NavLink href="#">Login</NavLink>
+        <SignUp href="#" style={{ marginLeft: "30px" }}>
+          Sign Up
+        </SignUp>
+      </NavLinksContainer>
+      <IconContainer style={{ marginLeft: "auto" }}>
+        <MenuIcon />
+      </IconContainer>
     </Container>
   );
 };
